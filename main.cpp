73743aab88cc1c8c83f6b9871adb4d44411afe29
Sil3ntUser <fredrimirc@myvuw.ac.nz>
2017-05-13 18:18:17 +1200

    Updated
    -added printing out what direction its going in
    -base for the Proportional control
    -

--- main.cpp
+++ main.cpp
@@ -19,56 +19,68 @@
  	
  	
 		int sum = 0;
- 		int i; // pixel
- 		char w; // whiteness of that pixel
- 		char s; // siginal of that pixel
+ 		int pix; // pixel
+ 		char white; // whiteness of that pixel
+ 		char signal; // siginal of that pixel
+		int num_white_pixels;
+		float k_P = 0.5;
+		int P_signal = 0;
  		
- 	for (i=0,i<320,i++){
- 		w = get_pixel(i,120,3);
-		if(w>127){ // sets pixel to white
- 		s=1;
+ 	for (pix=0,pix<320,pix++){
+ 		w = get_pixel(pix,120,3);
+		if(white>127){ // sets pixel to white
+ 		signal=1;
+		num_white_pixels++;	
 		}
 		else{ // sets pixel to black
- 		s=0;
+ 		signal=0;
 		}
-		if (s=0){// no white pixels
+		if (num_white_pixels=0){// no white pixels
 		reverse();
 		}
- 		sum = sum + (i-160)*s;
- 		}
- 	
+ 		sum = sum + (pix-160)*signal;
+ 		
+		P_signal = sum * k_P;
+		}
    
-   
+  	 
    
  
 	if (sum=0){ // line is in the middle
-		straight(int sum);
+		straight(int P_signal);
 	}
 	if (sum<0){ // line is to the left
-		turn_left(int sum);
+		turn_left(int P_signal);
 	}
 	if sum>0){ // line is to the right
-		turn_right(int sum);
+		turn_right(int P_signal);
 	}
  
  
-	int straight(){ // drives the robot straight ahead
+	int straight(P_signal){ // drives the robot straight ahead
+		print("Direction = Straight\n");
+		printf("Proportional Signal = %d\n" , P_signal);
 		set_motor(1, -60);
 		set_motor(2, 60);
 		sleep1(0,100);
 	}
  
-	int turn_left(sum){ // turns left
+	int turn_left(P_signal){ // turns left
+		print("Direction = Left\n");
+		printf("Proportional Signal = %d\n" , P_signal);
 		set_motor(1, 60);
 		set_motor(2, 60);
 		sleep1(0,100);
 	}
-	int turn_right(sum){ // turns right
+	int turn_right(P_signal){ // turns right
+		print("Direction = Right\n");
+		printf("Proportional Signal = %d\n" , P_signal);
 		set_motor(1, -60);
 		set_motor(2, -60);
 		sleep1(0,100);
 	}
-	int reverse (sum){ // reverses the robot
+	int reverse (){ // reverses the robot
+		print("Direction = Reverse\n");
 		set_motor(1, -60);
 		set_motor(2, 60);
 		sleep1(0,100);
