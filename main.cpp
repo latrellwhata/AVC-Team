252a17c72af881ef865edd7845e02b5ee5d2a122
Mirca Fredrick <fredrimirc@myvuw.ac.nz>
2017-05-14 22:10:49 +1200

    updated

--- main.cpp
+++ main.cpp
@@ -21,69 +21,46 @@
 		int sum = 0;
  		int pix; // pixel
  		char white; // whiteness of that pixel
- 		char signal; // siginal of that pixel
-		int num_white_pixels;
+ 		char sig; // siginal of that pixel
+		int num_white_pixels = 0;
 		float k_P = 0.5;
 		int P_signal = 0;
+		float multi = 0.4
+		
  		
  	for (pix=0,pix<320,pix++){
- 		w = get_pixel(pix,120,3);
+ 		white = get_pixel(pix,120,3);
 		if(white>127){ // sets pixel to white
- 		signal=1;
+ 		sig=1;
 		num_white_pixels++;	
 		}
 		else{ // sets pixel to black
- 		signal=0;
+ 		sig=0;
 		}
-		if (num_white_pixels=0){// no white pixels
-		reverse();
-		}
- 		sum = sum + (pix-160)*signal;
- 		
+		
+ 		sum = sum + (pix-160)*sig;
 		P_signal = sum * k_P;
 		}
-   
-  	 
-   
- 
-	if (sum=0){ // line is in the middle
-		straight(int P_signal);
-	}
-	if (sum<0){ // line is to the left
-		turn_left(int P_signal);
-	}
-	if sum>0){ // line is to the right
-		turn_right(int P_signal);
-	}
- 
- 
-	int straight(P_signal){ // drives the robot straight ahead
-		print("Direction = Straight\n");
-		printf("Proportional Signal = %d\n" , P_signal);
-		set_motor(1, -60);
-		set_motor(2, 60);
-		sleep1(0,100);
-	}
- 
-	int turn_left(P_signal){ // turns left
-		print("Direction = Left\n");
-		printf("Proportional Signal = %d\n" , P_signal);
-		set_motor(1, 60);
-		set_motor(2, 60);
-		sleep1(0,100);
-	}
-	int turn_right(P_signal){ // turns right
-		print("Direction = Right\n");
-		printf("Proportional Signal = %d\n" , P_signal);
-		set_motor(1, -60);
-		set_motor(2, -60);
-		sleep1(0,100);
-	}
-	int reverse (){ // reverses the robot
+		
+		int v1 = 70+P_signal*multi;
+		int v2 = 70-P_signal*multi;
+  		
+		if (num_white_pixels=0){ //if no white pixels, we lost the line
 		print("Direction = Reverse\n");
-		set_motor(1, -60);
-		set_motor(2, 60);
-		sleep1(0,100);
+		printf("Motor 1 = %f \n Motor 2 =%f", -v1, -v2 ;
+		set_motor(1, -v1);
+		set_motor(2, -v2);
+		}
+		else {
+		
+ 		printf("Direction = Foward\n");
+		printf("Proportional Signal = %d\n" , P_signal);
+		printf("Motor 1 = %f \n Motor 2 =%f", v1, v2 ;
+		set_motor(1,v1);
+		set_motor(2,v2);
+		}
 	}
-}	
+}
+
+ 	
 
