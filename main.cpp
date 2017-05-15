bf6a4cb7f1d4c8182902a98e803c25cf541fbd6f
Mirca Fredrick <fredrimirc@myvuw.ac.nz>
2017-05-15 13:58:05 +1200

    fixed errors

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 #include <time.h>
 #include "E101.h"
 
-	
- int straight();
- int turn_right();
- int turn_left();
- int reverse();
  
  
  int main(){
@@ -21,43 +16,42 @@
 		int sum = 0;
  		int pix; // pixel
  		char white; // whiteness of that pixel
- 		char sig; // siginal of that pixel
+ 		char sig[320]; // siginal of that pixel
 		int num_white_pixels = 0;
 		float k_P = 0.5;
 		int P_signal = 0;
-		float multi = 0.4
+		float multi = 0.4;
 		
  		
- 	for (pix=0,pix<320,pix++){
+ 	for (pix=0;pix<320;pix++){
  		white = get_pixel(pix,120,3);
 		if(white>127){ // sets pixel to white
- 		sig=1;
+ 		sig[pix]=1;
 		num_white_pixels++;	
 		}
 		else{ // sets pixel to black
- 		sig=0;
+ 		sig[pix]=0;
 		}
 		
- 		sum = sum + (pix-160)*sig;
+ 		sum = sum + (pix-160)*sig[pix];
 		P_signal = sum * k_P;
 		}
 		
 		int v1 = 70+P_signal*multi;
 		int v2 = 70-P_signal*multi;
   		
-		if (num_white_pixels=0){ //if no white pixels, we lost the line
-		print("Direction = Reverse\n");
-		printf("Motor 1 = %f \n Motor 2 =%f", -v1, -v2 ;
-		set_motor(1, -v1);
-		set_motor(2, -v2);
-		}
-		else {
-		
- 		printf("Direction = Foward\n");
+		if (num_white_pixels!=0){ //if no white pixels, we lost the line
+		printf("Direction = Foward\n");
 		printf("Proportional Signal = %d\n" , P_signal);
-		printf("Motor 1 = %f \n Motor 2 =%f", v1, v2 ;
+		printf("Motor 1 = %d \n Motor 2 =%d", v1, v2) ;
 		set_motor(1,v1);
 		set_motor(2,v2);
+		}
+		else {
+		printf("Direction = Reverse\n");
+		printf("Motor 1 = %d \n Motor 2 =%d", -v1, -v2) ;
+		set_motor(1, -v1);
+		set_motor(2, -v2);
 		}
 	}
 }
